ec19fc064e8a267d2f1452d6a28db3b562bc0063
Adrian Mata <cnevarez751@gmail.com>
2023-11-28 16:41:49 -0600

    oralegit add .!

--- validarUsuario.php
+++ validarUsuario.php
@@ -16,13 +16,10 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    //echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-    
-    
+  
     session_start();
 
     echo "Hola: " . $row["nombre"]; echo'<br>';
-   /*  echo "<br>Tu tipo de usuario es:  " . $row["jerarquia"]; */
     header("Refresh:0; url=./desgin/home.html");
 
   }
